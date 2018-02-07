ff757512d5e9bbfd76f69e1eb35180e7c1387e93
anchaleech <ch.anchalee@gmail.com>
2018-02-07 10:19:47 +0700

    chapter3-1

---
+++ chapter3-1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("====Zero Value=====")
+	var number int
+	var str string
+	var boolean bool
+	fmt.Println("number: %v\n", number)
+	fmt.Println("str: '%v\n", str)
+	fmt.Println("boolean: %v\n", boolean)
+}
