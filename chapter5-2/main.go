faba8994f5f4f12ce2abe5e1f000de8f544bde6c
anchaleech <ch.anchalee@gmail.com>
2018-02-13 12:09:27 +0700

    chapter5-4

--- chapter5-2/main.go
+++ chapter5-2/main.go
@@ -14,4 +14,28 @@ func main() {
 			fmt.Println(number)
 		}
 	}
-}
+
+	fmt.Println("======================")
+
+	for number := 1; number <= 100; number++ {
+		i := number%5
+		switch i {
+			case 0: fmt.Println("Zero")
+			case 1: fmt.Println("One")
+			case 2: fmt.Println("Two")
+			case 3: fmt.Println("Three")
+			case 4: fmt.Println("Four")
+			case 5: fmt.Println("Five")
+			default: fmt.Println("Unknown Number")
+			}
+		}
+
+	fmt.Println("======================")
+
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i%2 == 0: fmt.Println("even")
+		default: fmt.Println("Unknown Number")
+		}
+	}
+	}
