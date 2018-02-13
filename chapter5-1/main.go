4b7f158f015df4567f7f99ea916b31c17bd34858
anchaleech <ch.anchalee@gmail.com>
2018-02-13 10:07:57 +0700

    chapter5-2

--- chapter5-1/main.go
+++ chapter5-1/main.go
@@ -13,4 +13,14 @@ func main() {
 	fmt.Println("8")
 	fmt.Println("9")
 	fmt.Println("10")
+
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i = i + 1
+	}
 }
